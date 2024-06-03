51d79365ab16b7d61d34329a6b68987872ae0c1a
Hiroyuki OYAMA <oyama@module.jp>
2024-06-03 13:50:46 +0900

    Change deinit to the minimum necessary implementation.

--- src/deinit.c
+++ src/deinit.c
@@ -1,4 +1,4 @@
-/* Seamlessly share littlefs from Raspberry Pi Pico's onboard flash via USB
+/* Firmware to be disconnected in order to initialise the USB device.
  *
  * Copyright 2024, Hiroyuki OYAMA. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
@@ -6,14 +6,63 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
 #include <tusb.h>
-#include "filesystem/vfs.h"
 
 
 int main(void) {
-    tud_init(BOARD_TUD_RHPORT);
     stdio_init_all();
-    fs_init();
 
+    printf("\nShutdown the USB device ...");
     tud_disconnect();
     sleep_ms(250);
+    printf("ok\n");
+}
+
+bool tud_msc_test_unit_ready_cb(uint8_t lun) {
+    (void) lun;
+    return true;
+}
+
+void tud_msc_capacity_cb(uint8_t lun, uint32_t *block_count, uint16_t *block_size) {
+    (void) lun;
+    *block_count = 0;
+    *block_size  = 512;
+}
+
+void tud_msc_inquiry_cb(uint8_t lun, uint8_t vendor_id[8], uint8_t product_id[16], uint8_t product_rev[4]) {
+    (void) lun;
+
+    const char vid[] = "TinyUSB";
+    const char pid[] = "Mass Storage";
+    const char rev[] = "1.0";
+
+    memcpy(vendor_id  , vid, strlen(vid));
+    memcpy(product_id , pid, strlen(pid));
+    memcpy(product_rev, rev, strlen(rev));
+}
+
+int32_t tud_msc_read10_cb(uint8_t lun, uint32_t lba, uint32_t offset, void *buffer, uint32_t bufsize) {
+    (void)lun;
+    (void)lba;
+    (void)offset;
+    (void)buffer;
+
+    return (int32_t) bufsize;
+}
+
+int32_t tud_msc_write10_cb(uint8_t lun, uint32_t lba, uint32_t offset, uint8_t *buffer, uint32_t bufsize) {
+    (void)lun;
+    (void)lba;
+    (void)offset;
+    (void)buffer;
+
+    return bufsize;
+}
+
+int32_t tud_msc_scsi_cb(uint8_t lun, uint8_t const scsi_cmd[16], void *buffer, uint16_t bufsize) {
+    (void)lun;
+    (void)scsi_cmd;
+    (void)buffer;
+    (void)bufsize;
+
+    return 0;
 }
